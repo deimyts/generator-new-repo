46a8a7652953c0df4166bdbbd7a9f0994fef5641
Cameron Jones <deimyts@gmail.com>
2019-06-05 23:55:50 -0500

    refactor: reorder tests

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -7,34 +7,6 @@ describe('generator-new-repo:app', () => {
   const runGenerator = () =>
     helpers.run(path.join(__dirname, '../generators/app'));
 
-  describe('when a project name is provided', () => {
-    const projectName = 'testProject';
-
-    beforeAll(() => {
-      return runGenerator().withPrompts({ name: projectName });
-    });
-
-    it('creates the base files', () => {
-      assert.file(['.gitignore', 'package.json']);
-    });
-
-    it('configures the .gitignore file', () => {
-      assert.fileContent('.gitignore', 'node_modules');
-    });
-
-    it('uses the project name in package.json', () => {
-      assert.jsonFileContent('package.json', { name: projectName });
-    });
-
-    it('creates a folder with the same name as the project, and copies the files into it', () => {
-      assert.equal(path.basename(process.cwd()), projectName);
-    });
-
-    afterAll(() => {
-      helpers.cleanTestDirectory();
-    });
-  });
-
   describe('when no project name is given', () => {
     beforeAll(() => {
       return runGenerator();
@@ -51,6 +23,50 @@ describe('generator-new-repo:app', () => {
       const parentDir = path.join(currentDir, '..');
       assert.equal(parentDir, '/tmp');
       assert.notEqual(parentDir, currentDir);
+    });
+
+    afterAll(() => {
+      helpers.cleanTestDirectory();
+    });
+  });
+
+  describe('when a project name is provided', () => {
+    const projectName = 'testProject';
+
+    beforeAll(() => {
+      return runGenerator().withPrompts({ name: projectName });
+    });
+
+    it('uses the project name in package.json', () => {
+      assert.jsonFileContent('package.json', { name: projectName });
+    });
+
+    it('creates a folder with the same name as the project, and copies the files into it', () => {
+      assert.equal(path.basename(process.cwd()), projectName);
+    });
+
+    afterAll(() => {
+      helpers.cleanTestDirectory();
+    });
+  });
+
+  describe('when copying the base files', () => {
+    const projectName = 'testProject';
+
+    beforeAll(() => {
+      return runGenerator().withPrompts({ name: projectName });
+    });
+
+    it('copies the default config files', () => {
+      assert.file(['.gitignore', 'package.json']);
+    });
+
+    it('configures the .gitignore file', () => {
+      assert.fileContent('.gitignore', 'node_modules');
+    });
+
+    it('uses the project name in package.json', () => {
+      assert.jsonFileContent('package.json', { name: projectName });
     });
 
     afterAll(() => {
