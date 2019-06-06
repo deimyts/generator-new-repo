ee6cf5aa6355860d9884ad1e617d564fcdb5c63d
Cameron Jones <deimyts@gmail.com>
2019-06-05 20:43:44 -0500

    refactor: restructure tests

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -4,51 +4,54 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
 describe('generator-new-repo:app', () => {
-  beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ name: 'testProject' });
+  describe('when a project name is provided', () => {
+    beforeAll(() => {
+      return helpers
+        .run(path.join(__dirname, '../generators/app'))
+        .withPrompts({ name: 'testProject' });
+    });
+
+    it('creates the base files', () => {
+      assert.file(['.gitignore', 'package.json']);
+    })
+
+    it('configures the .gitignore file', () => {
+      assert.fileContent('.gitignore', 'node_modules');
+    });
+
+    it('uses the project name in package.json', () => {
+      assert.jsonFileContent('package.json', { name: 'testProject' })
+    })
+
+    it('creates a folder with the same name as the project, and copies the files into it', () => {
+      assert.equal(path.basename(process.cwd()), 'testProject');
+    })
+
+    afterAll(() => {
+      helpers.cleanTestDirectory()
+    })
+  })
+
+  describe('when no project name is given', () => {
+    beforeAll(() => {
+      return helpers
+        .run(path.join(__dirname, '../generators/app'))
+    });
+
+    it('names the project after the current working directory', () => {
+      assert.jsonFileContent('package.json', { name: path.basename(process.cwd())})
+    })
+
+    it('uses the current directory instead of creating a new one', () => {
+      const currentDir = process.cwd()
+      const parentDir = path.join(process.cwd(), '..')
+      assert.equal(parentDir, '/tmp')
+      assert.notEqual(parentDir, currentDir)
+    });
+
+    afterAll(() => {
+      helpers.cleanTestDirectory()
+    })
   });
-
-  it('creates the base files', () => {
-    assert.file(['.gitignore', 'package.json']);
-  })
-
-  it('configures the .gitignore file', () => {
-    assert.fileContent('.gitignore', 'node_modules');
-  });
-
-  it('uses the project name in package.json', () => {
-    assert.jsonFileContent('package.json', { name: 'testProject' })
-  })
-
-  it('creates a folder with the same name as the project, and copies the files into it', () => {
-    assert.equal(path.basename(process.cwd()), 'testProject');
-  })
-
-  afterAll(() => {
-    helpers.cleanTestDirectory()
-  })
 });
 
-describe('when no project name is given', () => {
-  beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, '../generators/app'))
-  });
-
-  it('names the project after the current working directory', () => {
-    assert.jsonFileContent('package.json', { name: path.basename(process.cwd())})
-  })
-
-  it('uses the current directory instead of creating a new one', () => {
-    const currentDir = process.cwd()
-    const parentDir = path.join(process.cwd(), '..')
-    assert.equal(parentDir, '/tmp')
-    assert.notEqual(parentDir, currentDir)
-  });
-
-  afterAll(() => {
-    helpers.cleanTestDirectory()
-  })
-});
